8de8a2d2f18d8d046328d389485f051c23e2ea36
para2ox <44606291+para2ox@users.noreply.github.com>
2026-02-06 01:54:36 +0500

    Update custom_settings.js

--- lampa/custom_settings.js
+++ lampa/custom_settings.js
@@ -18,7 +18,7 @@
 
         // Настраиваем кнопки на странице фильма в плагине LME Movie Enhancer
         Lampa.Storage.set('lme_showbutton', true);
-        Lampa.Storage.set('lme_buttonhide', '["view--trailer","shots-view-button","button--reaction","button--subscribe","button--options"]');
+        Lampa.Storage.set('lme_buttonhide', '["view--trailer","view--online","button--reaction","button--subscribe","button--options"]');
         Lampa.Storage.set('lme_buttonsort', '["view--online_mod","view--torrent","button--book","view--trailer","button--reaction","button--subscribe","button--options"]');
 
         // Скрываем пункты меню
