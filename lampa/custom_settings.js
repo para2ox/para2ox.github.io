f8a99b91c8fceba595e0e5813515f860e61b831f
para2ox <44606291+para2ox@users.noreply.github.com>
2026-02-05 21:31:43 +0500

    Update custom_settings.js

--- lampa/custom_settings.js
+++ lampa/custom_settings.js
@@ -3,8 +3,8 @@
 
     function applySettings() {
         // Преднастройки
-        Lampa.Storage.set('glass_style', true);
-        Lampa.Storage.set('source', 'SURS');
+        //Lampa.Storage.set('glass_style', true);
+        //Lampa.Storage.set('source', 'SURS');
         
         // Настраиваем HDrezka в Online Mod
         Lampa.Storage.set('online_mod_rezka2_cookie', 'dle_user_id=38372; dle_password=d8efa0170ea646402578694fe9ccf72e; dle_newpm=0; dle_user_token=cadfee4517c32230654c3c64a6002b0a; dle_user_taken=1');
@@ -15,13 +15,13 @@
         Lampa.Storage.set('online_mod_proxy_rezka2', true);
 
         // Скрываем пункты шапки
-        Lampa.Storage.set('head_filter_show_premium', false);
-        Lampa.Storage.set('head_filter_show_feed', false);
-        Lampa.Storage.set('head_filter_show_notice', false);
-        Lampa.Storage.set('head_filter_show_broadcast', false);
-        Lampa.Storage.set('head_filter_show_fullscreen', false);
-        Lampa.Storage.set('head_filter_show_reload', false);
-        Lampa.Storage.set('head_filter_show_blackfriday', false);
+        //Lampa.Storage.set('head_filter_show_premium', false);
+        //Lampa.Storage.set('head_filter_show_feed', false);
+        //Lampa.Storage.set('head_filter_show_notice', false);
+        //Lampa.Storage.set('head_filter_show_broadcast', false);
+        //Lampa.Storage.set('head_filter_show_fullscreen', false);
+        //Lampa.Storage.set('head_filter_show_reload', false);
+        //Lampa.Storage.set('head_filter_show_blackfriday', false);
 
         // Скрываем пункты меню
         Lampa.Storage.set('menu_hide', '["Лента","Фильмы","Мультфильмы","Сериалы","Персоны","Релизы","Аниме","Подписки","Расписание","Торренты","Спорт","Для детей","Информация","Консоль"]');
