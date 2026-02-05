4b980ea0ea345c1252c270cd4f71021b77585c53
para2ox <44606291+para2ox@users.noreply.github.com>
2026-02-05 18:44:13 +0500

    Update online_mod_login.js

--- lampa/online_mod_login.js
+++ lampa/online_mod_login.js
@@ -4,7 +4,7 @@
     function init() {
         // Выполнение запрошенных действий по установке настроек
         Lampa.Storage.set('online_mod_full_episode_title', 'true');
-        Lampa.Storage.set('online_mod_save_last_balanser', 'true');
+        Lampa.Storage.set('online_mod_save_last_balanser', 'false');
         Lampa.Storage.set('online_mod_rezka2_fix_stream', 'true');
         Lampa.Storage.set('online_mod_rezka2_status', 'true');
         Lampa.Storage.set('online_mod_rezka2_cookie', 'dle_user_id=38372; dle_password=d8efa0170ea646402578694fe9ccf72e; dle_newpm=0; dle_user_token=cadfee4517c32230654c3c64a6002b0a; dle_user_taken=1');
