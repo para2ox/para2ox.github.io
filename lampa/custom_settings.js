4c4e9f82aec40fc66f2b881f4da3f0cbbdbd2d0f
para2ox <44606291+para2ox@users.noreply.github.com>
2026-02-05 20:53:27 +0500

    Update custom_settings.js

--- lampa/custom_settings.js
+++ lampa/custom_settings.js
@@ -2,8 +2,11 @@
     'use strict';
 
     function applySettings() {
-        // --- ТВОИ НАСТРОЙКИ ---
+        // Преднастройки
+        Lampa.Storage.set('glass_style', true);
+        Lampa.Storage.set('source', 'SURS');
         
+        // Настраиваем HDrezka в Online Mod
         Lampa.Storage.set('online_mod_rezka2_cookie', 'dle_user_id=38372; dle_password=d8efa0170ea646402578694fe9ccf72e; dle_newpm=0; dle_user_token=cadfee4517c32230654c3c64a6002b0a; dle_user_taken=1');
         Lampa.Storage.set('online_mod_balanser', 'rezka2');
         Lampa.Storage.set('online_mod_save_last_balanser', true);
@@ -11,7 +14,7 @@
         Lampa.Storage.set('online_mod_rezka2_fix_stream', true);
         // Lampa.Storage.set('online_mod_proxy_rezka2', true);
 
-        // Скрываем пункты через настройки Lampa
+        // Скрываем пункты шапки
         Lampa.Storage.set('head_filter_show_premium', false);
         Lampa.Storage.set('head_filter_show_feed', false);
         Lampa.Storage.set('head_filter_show_notice', false);
@@ -19,16 +22,14 @@
         Lampa.Storage.set('head_filter_show_fullscreen', false);
         Lampa.Storage.set('head_filter_show_reload', false);
         Lampa.Storage.set('head_filter_show_blackfriday', false);
-        
+
+        // Скрываем пункты меню
         Lampa.Storage.set('menu_hide', '["Лента","Фильмы","Мультфильмы","Сериалы","Персоны","Релизы","Аниме","Подписки","Расписание","Торренты","Спорт","Для детей","Информация","Консоль"]');
 
-        // --- CSS ИНЪЕКЦИЯ (Исправлено для совместимости с TV) ---
+        // --- CSS ИНЪЕКЦИЯ ---
         
         // Используем обычные кавычки и одну строку, чтобы не ломать старые браузеры
-        var css = '.open--premium, .open--feed, .notice--icon, .open--broadcast, .full--screen, .m-reload-screen, .black-friday__button,
-            .menu .menu__split, .menu li.menu__item[data-action="settings"], .menu li.menu__item[data-action="about"], .menu li.menu__item[data-action="console"], .menu li.menu__item[data-action="edit"] {
-                display: none !important;
-        }';
+        var css = '.open--premium, .open--feed, .notice--icon, .open--broadcast, .full--screen, .m-reload-screen, .black-friday__button, .menu .menu__split, .menu li.menu__item[data-action="settings"], .menu li.menu__item[data-action="about"], .menu li.menu__item[data-action="console"], .menu li.menu__item[data-action="edit"] {display: none !important;}';
 
         var style = document.createElement('style');
         style.type = 'text/css';
