6e6e6959e1b3c43a657cf92ec54de10a51ff0e84
para2ox <44606291+para2ox@users.noreply.github.com>
2026-02-05 20:05:30 +0500

    Update custom_settings.js

--- lampa/custom_settings.js
+++ lampa/custom_settings.js
@@ -2,26 +2,28 @@
     'use strict';
 
     function applySettings() {
-        // ВАЖНО: Передаем true/false как boolean (без кавычек), а не как строки.
+        // Авторизация в HDrezka
+        Lampa.Storage.set('online_mod_rezka2_cookie', 'dle_user_id=38372; dle_password=d8efa0170ea646402578694fe9ccf72e; dle_newpm=0; dle_user_token=cadfee4517c32230654c3c64a6002b0a; dle_user_taken=1');
+        
+        // Ставим HDrezka балансером по умолчанию
+        Lampa.Storage.set('online_mod_balanser', 'rezka2');
+        
+        // Включаем сохранение последнего балансера
+        Lampa.Storage.set('online_mod_save_last_balanser', true);
         
         // Включаем отображение полного названия эпизода
         Lampa.Storage.set('online_mod_full_episode_title', true);
         
-        // Отключаем сохранение последнего балансера
-        Lampa.Storage.set('online_mod_save_last_balanser', true);
-        
         // Фикс видеопотока (важно для некоторых провайдеров)
         Lampa.Storage.set('online_mod_rezka2_fix_stream', true);
+        
+        // (bullshit?) Включаем проксирование для Rezka. БЕЗ ЭТОГО куки не отправятся на TV/PC, так как браузер блокирует заголовок Cookie
+        // Lampa.Storage.set('online_mod_proxy_rezka2', true);
 
-        Lampa.Storage.set('online_mod_balanser', 'rezka2');
-        
-        // Включаем проксирование для Rezka
-        // БЕЗ ЭТОГО куки не отправятся на TV/PC, так как браузер блокирует заголовок Cookie
-        Lampa.Storage.set('online_mod_proxy_rezka2', true);
-        
-        // Установка куки (строка остается строкой)
-        // Убедитесь, что эти куки свежие и получены с того же IP, с которого смотрите
-        Lampa.Storage.set('online_mod_rezka2_cookie', 'dle_user_id=38372; dle_password=d8efa0170ea646402578694fe9ccf72e; dle_newpm=0; dle_user_token=cadfee4517c32230654c3c64a6002b0a; dle_user_taken=1');
+        //////
+
+        // Скрываем лишние пункты в боковом меню
+        Lampa.Storage.set('menu_hide', '["Лента","Фильмы","Мультфильмы","Сериалы","Персоны","Релизы","Аниме","Подписки","Расписание","Торренты","Спорт","Для детей","Информация","Консоль"]');
     }
 
     if (window.appready) {
