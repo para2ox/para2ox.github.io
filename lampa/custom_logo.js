3bcf6589c6d8370214747c0f4b2e9ce7505b417a
para2ox <44606291+para2ox@users.noreply.github.com>
2026-02-07 04:15:53 +0500

    Update custom_logo.js

--- lampa/custom_logo.js
+++ lampa/custom_logo.js
@@ -1,22 +1,26 @@
-function pluginSaturnLogo(){
+function pluginSauronLogo(){
     function add(){
-        // Создаем HTML контейнер с новым SVG
-        let logo = $(`<div class="head__logo-icon head__logo-saturn">
-            <svg width="100%" height="100%" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-                <radialGradient id="ZE9xEe17dPUapcT~byNjua" cx="20.769" cy="33.403" r="9.787" gradientTransform="rotate(-45.001 24 23.647)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e5c494" stop-opacity=".2"/><stop offset="1" stop-color="#c1ae84"/></radialGradient><path fill="url(#ZE9xEe17dPUapcT~byNjua)" d="M31.348,30.994c-4.677,4.677-7.931,6.539-9.88,6.62c0.885,0.179,1.783,0.272,2.682,0.278c1.806-0.058,4.054-0.745,8.703-5.393c3.863-3.863,5.062-6.03,5.355-7.703c0.075-1.165,0.014-2.338-0.201-3.489C37.779,23.284,35.829,26.512,31.348,30.994z"/><radialGradient id="ZE9xEe17dPUapcT~byNjub" cx="20.524" cy="20.7" r="16.227" gradientTransform="rotate(-45.001 24 24.147)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e1c08d"/><stop offset=".431" stop-color="#c0a47d"/><stop offset="1" stop-color="#5e6551"/></radialGradient><circle cx="24.25" cy="23.896" r="14" fill="url(#ZE9xEe17dPUapcT~byNjub)"/><radialGradient id="ZE9xEe17dPUapcT~byNjuc" cx="22.13" cy="13.827" r="10.436" gradientTransform="rotate(-45.001 24 24.147)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e5c494" stop-opacity=".2"/><stop offset=".992" stop-color="#fabf59" stop-opacity=".5"/></radialGradient><path fill="url(#ZE9xEe17dPUapcT~byNjuc)" d="M10.347,22.305c1.676-0.889,4.19-2.879,6.761-5.45c2.619-2.619,4.635-5.179,5.498-6.854C16.172,10.755,11.077,15.865,10.347,22.305z"/><radialGradient id="ZE9xEe17dPUapcT~byNjud" cx="22.389" cy="14.415" r="10.766" gradientTransform="rotate(-45.001 24 24.147)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e5c494" stop-opacity=".2"/><stop offset=".992" stop-color="#f78f8f" stop-opacity=".5"/></radialGradient><path fill="url(#ZE9xEe17dPUapcT~byNjud)" d="M10.384,24.508c-0.065-0.065-0.092-0.166-0.101-0.282l-0.016,0.011c0.029,1.216,0.212,2.393,0.533,3.513l0.033-0.016c0.01,0.014,0.013,0.034,0.026,0.046c0.839,0.839,5.384-2.345,10.151-7.113c4.767-4.767,7.952-9.312,7.113-10.151c-0.042-0.042-0.1-0.066-0.159-0.088l0.013-0.014c-1.112-0.307-2.276-0.484-3.48-0.505l-0.025,0.025c0.166-0.008,0.296,0.025,0.379,0.108c0.703,0.703-1.965,4.511-5.96,8.506S11.088,25.211,10.384,24.508z"/><radialGradient id="ZE9xEe17dPUapcT~byNjue" cx="21.583" cy="21.187" r="9.97" gradientTransform="rotate(-45.001 24 24.147)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e5c494" stop-opacity=".2"/><stop offset="1" stop-color="#936c4e" stop-opacity=".5"/></radialGradient><path fill="url(#ZE9xEe17dPUapcT~byNjue)" d="M11.475,29.57c-0.06-0.06-0.101-0.137-0.125-0.229l-0.001,0.001c0.42,0.993,0.962,1.919,1.589,2.779l0.011-0.006c1.044,0.906,6.178-2.655,11.538-8.015c5.251-5.251,8.768-10.279,8.057-11.463c-0.795-0.587-1.65-1.093-2.561-1.503c0.808,1.021-2.545,5.844-7.581,10.881C17.298,27.118,12.406,30.501,11.475,29.57z"/><radialGradient id="ZE9xEe17dPUapcT~byNjuf" cx="21.973" cy="20.86" r="13.985" gradientTransform="rotate(-45.001 24 24.147)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e5c494" stop-opacity=".2"/><stop offset="1" stop-color="#534741" stop-opacity=".5"/></radialGradient><path fill="url(#ZE9xEe17dPUapcT~byNjuf)" d="M13.51,32.872c1.076,1.287,2.382,2.372,3.851,3.204c2.167-0.083,6.763-3.144,11.327-7.707c4.625-4.625,7.707-9.285,7.709-11.413c-0.873-1.523-2.018-2.868-3.377-3.963l-0.009,0.009c0.03,0.023,0.07,0.037,0.097,0.064c1.348,1.348-1.975,6.856-7.421,12.302C20.383,30.671,15.033,33.947,13.51,32.872z"/><radialGradient id="ZE9xEe17dPUapcT~byNjug" cx="24.321" cy="33.729" r="8.773" gradientTransform="rotate(-45.001 24 24.147)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e5c494" stop-opacity=".2"/><stop offset="1" stop-color="#534741" stop-opacity=".5"/></radialGradient><path fill="url(#ZE9xEe17dPUapcT~byNjug)" d="M20.604,37.326l-0.045,0.062c1.178,0.322,2.411,0.508,3.691,0.508c0.432,0,0.858-0.026,1.28-0.065c1.719-0.479,4.569-2.464,7.385-5.281c2.795-2.795,4.77-5.62,5.268-7.344c0.04-0.432,0.066-0.868,0.066-1.311c0-1.235-0.176-2.427-0.476-3.568l-0.017,0.017c0.628,1.241-2.326,5.674-6.849,10.197C26.247,35.203,21.681,38.201,20.604,37.326z"/><linearGradient id="ZE9xEe17dPUapcT~byNjuh" x1="2.149" x2="41.468" y1="17.264" y2="30.285" gradientTransform="rotate(-45.001 24 24.147)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#c7b299"/><stop offset="1" stop-color="#aa8765"/></linearGradient><path fill="url(#ZE9xEe17dPUapcT~byNjuh)" d="M9.058,38.847c1.228,1.228,8.859-4.413,17.045-12.598S39.929,10.432,38.701,9.205c-0.651-0.651-3.112,0.642-6.491,3.208c-0.224-0.156-0.459-0.298-0.692-0.441c5.096-3.98,8.752-5.834,9.593-4.993c1.347,1.347-4.216,9.911-14.353,20.047S8.058,42.725,6.712,41.378c-0.879-0.879,1.19-4.838,5.554-10.302c0.142,0.237,0.283,0.476,0.439,0.704C9.84,35.46,8.37,38.159,9.058,38.847z M30.176,11.233c0.297,0.14,0.585,0.297,0.87,0.457c4.986-3.942,9.167-6.316,10.419-5.065c2.079,2.079-5.843,12.245-14.353,20.754S8.437,43.811,6.358,41.732c-1.31-1.31,1.354-5.834,5.632-11.129c-0.153-0.279-0.302-0.561-0.437-0.851C5.274,37.349,1.884,43.531,3.5,45.146c2.251,2.251,13.366-5.214,24.826-16.674C39.786,17.012,47.251,5.897,45,3.646C43.425,2.072,37.51,5.256,30.176,11.233z"/></svg>
+        // Создаем HTML контейнер с картинкой (PNG)
+        // Используем класс head__logo-icon для сохранения базового поведения Lampa
+        // И добавляем свой класс head__logo-sauron для стилизации
+        let logo = $(`<div class="head__logo-icon head__logo-sauron">
+            <img src="https://para2ox.github.io/lampa/sauron.png" alt="Sauron Logo">
         </div>`);
 
-        // Добавляем минимальные стили, чтобы иконка была нужного размера
-        // Стандартная иконка в Lampa имеет высоту около 2.5em
+        // Добавляем стили, чтобы картинка была адекватного размера.
+        // 'height: 2.3em' - это высота стандартного лого Lampa.
+        // 'width: auto' сохраняет пропорции картинки.
         $('body').append(`<style>
-            .head__logo-saturn svg {
+            .head__logo-sauron img {
                 height: 2.3em;
                 width: auto;
                 display: block;
+                /* Опционально: можно добавить небольшой отступ, если нужно */
+                /* margin-top: 0.1em; */
             }
         </style>`);
 
-        // Заменяем оригинальную иконку
+        // Заменяем оригинальную иконку на нашу новую
         $('.head .head__logo-icon').replaceWith(logo);
 
         // Восстанавливаем функционал клика для открытия меню
@@ -25,6 +29,7 @@ function pluginSaturnLogo(){
         })
     }
 
+    // Стандартная проверка готовности приложения Lampa
     if(window.appready) add()
     else{
         Lampa.Listener.follow('app', function (e) {
@@ -33,7 +38,8 @@ function pluginSaturnLogo(){
     }
 }
 
-if(!window.plugin_saturn_ready) {
-    window.plugin_saturn_ready = true;
-    pluginSaturnLogo();
+// Защита от повторного запуска плагина
+if(!window.plugin_sauron_ready) {
+    window.plugin_sauron_ready = true;
+    pluginSauronLogo();
 }
