e96539aaf66a24de2e4ae20281cd19902539da0f
para2ox <44606291+para2ox@users.noreply.github.com>
2026-02-07 01:14:32 +0500

    Update custom_settings.js

--- lampa/custom_settings.js
+++ lampa/custom_settings.js
@@ -48,7 +48,7 @@
         css += '.head__title {visibility: hidden;} ';
 
         // Страница фильма
-        css += '.wrap .full-start__background {left: -55px !important; width: 100vw !important; max-width: 100vw !important;} ';
+        css += '.wrap .full-start__background {left: -5em !important; width: 100vw !important; max-width: 100vw !important;} ';
 
         
         var style = document.createElement('style');
