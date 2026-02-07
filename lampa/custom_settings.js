4e1f9acebe176203c141f19c83072d843e8fb137
para2ox <44606291+para2ox@users.noreply.github.com>
2026-02-07 19:05:23 +0500

    Update custom_settings.js

--- lampa/custom_settings.js
+++ lampa/custom_settings.js
@@ -6,7 +6,7 @@
         Lampa.Storage.set('start_page', 'main');
         Lampa.Storage.set('source', 'SURS');
         Lampa.Storage.set('glass_style', true);
-        Lampa.Storage.set('video_quality_default', '720');
+        Lampa.Storage.set('video_quality_default', '1080');
 
         // Настраиваем плагин Online Mod
         Lampa.Storage.set('online_mod_rezka2_cookie', 'dle_user_id=38372; dle_password=d8efa0170ea646402578694fe9ccf72e; dle_newpm=0; dle_user_token=cadfee4517c32230654c3c64a6002b0a; dle_user_taken=1');
