64201c164a5c01f47c9dce0680657f969c566356
para2ox <44606291+para2ox@users.noreply.github.com>
2026-04-07 20:56:30 +0500

    Update settings.js

--- lampa/settings.js
+++ lampa/settings.js
@@ -10,7 +10,7 @@
                 'https://ipavlin98.github.io/lmp-plugins/int.js',
                 'https://darkestclouds.github.io/plugins/applecation/applecation.js',
                 'http://cub.red/plugin/tmdb-proxy',
-                'https://nb557.github.io/plugins/online_mod.js',
+                'http://bwa.ad/rc',
                 'https://lampame.github.io/main/lme.js',
                 'https://ipavlin98.github.io/lmp-plugins/series-progress-fix.js',
                 'https://nb557.github.io/plugins/free.js',
@@ -45,16 +45,16 @@
         // Lampa.Storage.set('background_type', 'simple');
         // Lampa.Storage.set('black_style', true);
 
-        //Lampa.Storage.set('agree_installation', true);
+        // Настраиваем BWA (http://bwa.ad/rc)
         Lampa.Storage.set('bwaesgcmkey', 'NkL56zBHtwCjcOuE4RQmXMcVr2HhIh4cDEdLqknju7w=');
+        //Lampa.Storage.set('agree_installation', true);
 
-        // Настраиваем плагин Online Mod
+        /* // Настраиваем плагин Online Mod (https://nb557.github.io/plugins/online_mod.js)
         Lampa.Storage.set('online_mod_rezka2_cookie', 'dle_user_id=38372; dle_password=d8efa0170ea646402578694fe9ccf72e; dle_newpm=0; dle_user_token=cadfee4517c32230654c3c64a6002b0a; dle_user_taken=1');
         Lampa.Storage.set('online_mod_balanser', 'rezka2');
         Lampa.Storage.set('online_mod_save_last_balanser', true);
         Lampa.Storage.set('online_mod_full_episode_title', true);
-        Lampa.Storage.set('online_mod_rezka2_fix_stream', true);
-        // Lampa.Storage.set('online_mod_proxy_rezka2', false);
+        Lampa.Storage.set('online_mod_rezka2_fix_stream', true); */
 
         // Настраиваем плагин Applecation
         Lampa.Storage.set('applecation_text_scale', '120');
@@ -80,7 +80,7 @@
 
         // Настраиваем кнопки на странице фильма в плагине LME Movie Enhancer
         Lampa.Storage.set('lme_showbutton', true);
-        Lampa.Storage.set('lme_buttonhide', '["view--trailer","view--online","view--online_mod","button--reaction","button--subscribe","button--options"]');
+        Lampa.Storage.set('lme_buttonhide', '["view--trailer","view--online_mod","button--reaction","button--subscribe","button--options"]');
         Lampa.Storage.set('lme_buttonsort', '["view--online","view--online_mod","view--torrent","view--rutube_trailer","button--book"]');
 
         // Скрываем и сортируем пункты меню
