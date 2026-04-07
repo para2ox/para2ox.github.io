411247c6d466537847deef3d0d5372c1e05b3608
para2ox <44606291+para2ox@users.noreply.github.com>
2026-04-07 21:17:08 +0500

    Update settings_clean.js

--- lampa/settings_clean.js
+++ lampa/settings_clean.js
@@ -23,13 +23,16 @@
         // Lampa.Storage.set('background_type', 'simple');
         // Lampa.Storage.set('black_style', true);
 
-        // Настраиваем плагин Online Mod
+        // Настраиваем BWA (http://bwa.ad/rc)
+        Lampa.Storage.set('bwaesgcmkey', 'NkL56zBHtwCjcOuE4RQmXMcVr2HhIh4cDEdLqknju7w=');
+        //Lampa.Storage.set('agree_installation', true);
+
+        /* // Настраиваем плагин Online Mod (https://nb557.github.io/plugins/online_mod.js)
         Lampa.Storage.set('online_mod_rezka2_cookie', 'dle_user_id=38372; dle_password=d8efa0170ea646402578694fe9ccf72e; dle_newpm=0; dle_user_token=cadfee4517c32230654c3c64a6002b0a; dle_user_taken=1');
         Lampa.Storage.set('online_mod_balanser', 'rezka2');
         Lampa.Storage.set('online_mod_save_last_balanser', true);
         Lampa.Storage.set('online_mod_full_episode_title', true);
-        Lampa.Storage.set('online_mod_rezka2_fix_stream', true);
-        // Lampa.Storage.set('online_mod_proxy_rezka2', false);
+        Lampa.Storage.set('online_mod_rezka2_fix_stream', true); */
 
         // Настраиваем плагин Applecation
         Lampa.Storage.set('applecation_text_scale', '120');
@@ -56,7 +59,7 @@
         // Настраиваем кнопки на странице фильма в плагине LME Movie Enhancer
         Lampa.Storage.set('lme_showbutton', true);
         Lampa.Storage.set('lme_buttonhide', '["view--trailer","view--online","button--reaction","button--subscribe","button--options"]');
-        Lampa.Storage.set('lme_buttonsort', '["view--online_mod","view--torrent","view--rutube_trailer","button--book"]');
+        Lampa.Storage.set('lme_buttonsort', '["view--online:443719427","view--online_mod","view--torrent","view--rutube_trailer","button--book"]');
 
         // Скрываем и сортируем пункты меню
         Lampa.Storage.set('menu_hide', '["Подборки","Каталог","Лента","Фильмы","Мультфильмы","Сериалы","Персоны","Релизы","Аниме","Подписки","Расписание","Торренты","Спорт","Для детей","Shots","Torrent Manager"]');
@@ -105,6 +108,9 @@
 
 .wrap__content .activity__body > div > div {
     padding: 1em 2em !important;
+}
+.new-interface-info__body {
+    padding: 0.2em 1.5em 0 !important;
 }
 
 .full-start-new__buttons {
