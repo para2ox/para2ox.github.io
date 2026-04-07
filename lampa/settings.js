e0c23d26702dd2ed1b75c7c56c9fca400788b2a8
para2ox <44606291+para2ox@users.noreply.github.com>
2026-04-07 20:58:24 +0500

    Update settings.js

--- lampa/settings.js
+++ lampa/settings.js
@@ -80,8 +80,8 @@
 
         // Настраиваем кнопки на странице фильма в плагине LME Movie Enhancer
         Lampa.Storage.set('lme_showbutton', true);
-        Lampa.Storage.set('lme_buttonhide', '["view--trailer","view--online_mod","button--reaction","button--subscribe","button--options"]');
-        Lampa.Storage.set('lme_buttonsort', '["view--online","view--online_mod","view--torrent","view--rutube_trailer","button--book"]');
+        Lampa.Storage.set('lme_buttonhide', '["view--trailer","view--online","button--reaction","button--subscribe","button--options"]');
+        Lampa.Storage.set('lme_buttonsort', '["view--online:443719427","view--online_mod","view--torrent","view--rutube_trailer","button--book"]');
 
         // Скрываем и сортируем пункты меню
         Lampa.Storage.set('menu_hide', '["Подборки","Каталог","Лента","Фильмы","Мультфильмы","Сериалы","Персоны","Релизы","Аниме","Подписки","Расписание","Торренты","Спорт","Для детей","Shots","Torrent Manager"]');
