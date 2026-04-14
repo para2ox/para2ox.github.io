146427e78002c65b0f6d12c98c49813bd3adf31d
para2ox <44606291+para2ox@users.noreply.github.com>
2026-04-14 21:33:57 +0500

    Update test2.js

--- lampa/test2.js
+++ lampa/test2.js
@@ -39,6 +39,7 @@
             css += '.full-start-new__buttons { padding-top: 1em; } ';
             css += '.items-line__head { margin-top: 3.5em; } ';
             css += '.scroll--horizontal .scroll__content { margin-top: 0.5em; } ';
+            css += '.new-interface-info__body:not(:has(.visible)) { display: none; } ';
             
             // Переменные
             css += ':root { --menu-bg: rgba(20, 20, 23, 0.75); --menu-bg-hover: rgba(20, 20, 23, 0.95); --menu-width-collapsed: 72px; --menu-width-expanded: 200px; --accent-color: #e50914; --text-color: #e8e8e8; --text-color-active: #ffffff; --blur-strength: 10px; --transition-speed: 0.4s cubic-bezier(0.25, 0.8, 0.25, 1); } ';
