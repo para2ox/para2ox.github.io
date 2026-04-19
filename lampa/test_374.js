0642b086b50254004edae586f1dc572f0e417f66
para2ox <44606291+para2ox@users.noreply.github.com>
2026-04-19 20:37:47 +0500

    Update test_374.js

--- lampa/test_374.js
+++ lampa/test_374.js
@@ -1,1 +1,27 @@
+(function () {
+     var garbage_collector = {
+      name: 'Сборщик хлама',
+      version: '1.0',
+      description: 'Убирает не нужные разделы как торренты и лента'
+    };
+    function replaceAdText() {
+        if (window.ru && window.ru.ad) {
+            window.ru.ad = 'Приятного чаяпития'; // Замените на нужное слово
+        }
+    }
 
+    // Первоначальная замена
+    replaceAdText();
+
+    // Интервал для периодической замены (каждые 500 мс)
+    setInterval(replaceAdText, 500);
+
+    // Используем MutationObserver для отслеживания изменений объекта window.ru
+    var observer = new MutationObserver(replaceAdText);
+
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+
+})();
